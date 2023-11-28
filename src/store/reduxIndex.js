4f9390672b8bb72b4d1d89d3c432d29b776ded6d
nihar078 <nihar078@gmail.com>
2023-11-28 17:48:41 +0530

    Redux_state_correctly

--- src/store/reduxIndex.js
+++ src/store/reduxIndex.js
@@ -3,8 +3,15 @@ import { createStore } from "redux";
 const initialState = {counter: 0, showCounter: true}
 const counterReducer = (state = initialState, action)=>{
     if(action.type === "increment"){
+        // //(unmuted this ..this can lead to debug)
+        // state.counter++   //(unmuted this ..this can lead to debug)
+        // return{
+        //     counter : state.counter,
+        //     showCounter: state.showCounter
+        // }
+        // (this is original state)
         return{
-            counter : state.counter + 1,
+            counter: state.counter + 1,
             showCounter: state.showCounter
         }
     }
