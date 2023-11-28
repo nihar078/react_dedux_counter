f85bc92082c102c088ca519e45a7b597ca85ac0d
nihar078 <nihar078@gmail.com>
2023-11-28 17:41:05 +0530

    multiple_state_working

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,37 +1,44 @@
-import { useDispatch, useSelector } from 'react-redux';
-import classes from './Counter.module.css';
-
+import { useDispatch, useSelector } from "react-redux";
+import classes from "./Counter.module.css";
 
 const Counter = () => {
-  const dispatch = useDispatch()
-  const counter = useSelector(state => state.counter)
-  
-  const toggleCounterHandler = () => {};
-  const incrementHandler = () =>{
-    dispatch({type: "increment"})
-  }
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
-  const increaseHandler = ()=>{
-    dispatch({type: "increase", amount: 3})
-  }
-  const decrementHandler = () =>{
-    dispatch({type: "decrement"})
-  }
-  const incrementBy5Handler = () =>{
-    dispatch({type: "incrementBy5"})
-  }
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
 
-  const decrementBy5Handler = ()=>{
-    dispatch({type: "decrementBy5"})
-  }
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 3 });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+  const incrementBy5Handler = () => {
+    dispatch({ type: "incrementBy5" });
+  };
+
+  const decrementBy5Handler = () => {
+    dispatch({ type: "decrementBy5" });
+  };
+
+  const incrementBy2Handler = () => {
+    dispatch({ type: "incrementBy2", amount: 2 });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increse By 3</button>
+        <button onClick={incrementBy2Handler}>Increment By 2</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <div>
